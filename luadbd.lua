6ef67449a1d5f31518f39e767b39c8873f18dd6b
Ferronn <ferronn@ferronn.dev>
2021-08-02 17:19:50 -0500

    upvalue all the lpeg functions

--- luadbd.lua
+++ luadbd.lua
@@ -1,31 +1,31 @@
 local lpeg = require('lpeg')
+local C, Cg, Ct, P, R, S = lpeg.C, lpeg.Cg, lpeg.Ct, lpeg.P, lpeg.R, lpeg.S
 
-local lit = lpeg.P
-local coltype = lit('int') + lit('string') + lit('float') + lit('locstring')
-local sym = lpeg.R('az', 'AZ') * lpeg.R('az', 'AZ', '09')^0
-local fkey = lit('<') * sym * lit('::') * sym * lit('>')
-local skiptoeol = (1 - lpeg.S('\n'))^0
-local commenteol = (lit(' // ') * skiptoeol)^-1 * lit('\n')
-local column = lpeg.Ct(
-    lpeg.Cg(lpeg.C(coltype), 'type') *
+local coltype = P('int') + P('string') + P('float') + P('locstring')
+local sym = R('az', 'AZ') * R('az', 'AZ', '09')^0
+local fkey = P('<') * sym * P('::') * sym * P('>')
+local skiptoeol = (1 - S('\n'))^0
+local commenteol = (P(' // ') * skiptoeol)^-1 * P('\n')
+local column = Ct(
+    Cg(C(coltype), 'type') *
     fkey^-1 *
-    lit(' ') *
-    lpeg.Cg(sym, 'name') *
-    lit('?')^-1 *
+    P(' ') *
+    Cg(sym, 'name') *
+    P('?')^-1 *
     commenteol)
-local build = lit('BUILD ') * lpeg.C((lpeg.R('09') + lpeg.S('.-, '))^0) * lit('\n')
-local buildcol = lpeg.Ct(lpeg.Cg(sym, 'name') * commenteol)
-local version = lpeg.Ct(
-    lit('\n') *
-    lpeg.Cg(lit('LAYOUT ') * lpeg.C(lpeg.R('09', 'AF')^8) * lit('\n'), 'layout')^-1 *
-    lpeg.Cg(lpeg.Ct(build^0), 'builds') *
-    (lit('COMMENT ') * skiptoeol * lit('\n'))^-1 *
-    lpeg.Cg(lpeg.Ct(buildcol^0), 'columns'))
-local dbd = lpeg.Ct(
-    lit('COLUMNS\n') *
-    lpeg.Cg(lpeg.Ct(column^0), 'columns') *
-    lpeg.Cg(lpeg.Ct(version^0), 'versions') *
-    -lpeg.P(1))
+local build = P('BUILD ') * C((R('09') + S('.-, '))^0) * P('\n')
+local buildcol = Ct(Cg(sym, 'name') * commenteol)
+local version = Ct(
+    P('\n') *
+    Cg(P('LAYOUT ') * C(R('09', 'AF')^8) * P('\n'), 'layout')^-1 *
+    Cg(Ct(build^0), 'builds') *
+    (P('COMMENT ') * skiptoeol * P('\n'))^-1 *
+    Cg(Ct(buildcol^0), 'columns'))
+local dbd = Ct(
+    P('COLUMNS\n') *
+    Cg(Ct(column^0), 'columns') *
+    Cg(Ct(version^0), 'versions') *
+    -P(1))
 
 local parse = function(s)
   return dbd:match(s)
