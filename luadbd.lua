db24594a4a307dfa104dc2229fa6976d0cff7a8a
Ferronn <ferronn@ferronn.dev>
2021-08-02 19:23:46 +0000

    rudimentary version parsing

--- luadbd.lua
+++ luadbd.lua
@@ -4,16 +4,25 @@ local lit = lpeg.P
 local coltype = lit('int') + lit('string') + lit('float') + lit('locstring')
 local sym = lpeg.R('az', 'AZ') * lpeg.R('az', 'AZ', '09')^0
 local fkey = lit('<') * sym * lit('::') * sym * lit('>')
-local column =
-    lpeg.Ct(lpeg.Cg(lpeg.C(coltype), 'type') *
+local column = lpeg.Ct(
+    lpeg.Cg(lpeg.C(coltype), 'type') *
     fkey^-1 *
     lit(' ') *
     lpeg.Cg(sym, 'name') *
     lit('?')^-1 *
     (lit(' // ') * (1 - lpeg.S('\n'))^0)^-1 *
     lit('\n'))
-local columns = lit('COLUMNS\n') * lpeg.Ct(column^0)
-local dbd = lpeg.Ct(lpeg.Cg(columns, 'columns') * -lpeg.P(1))
+local build = lit('BUILD ') * lpeg.C((lpeg.R('09') + lpeg.S('.-, '))^0) * lit('\n')
+local buildcol = lpeg.Ct(lpeg.Cg(sym, 'name') * lit('\n'))
+local version = lpeg.Ct(
+    lit('\n') *
+    lpeg.Cg(lpeg.Ct(build^0), 'builds') *
+    lpeg.Cg(lpeg.Ct(buildcol^0), 'columns'))
+local dbd = lpeg.Ct(
+    lit('COLUMNS\n') *
+    lpeg.Cg(lpeg.Ct(column^0), 'columns') *
+    lpeg.Cg(lpeg.Ct(version^0), 'versions') *
+    -lpeg.P(1))
 
 local parse = function(s)
   return dbd:match(s)
--- luadbd_spec.lua
+++ luadbd_spec.lua
@@ -1,4 +1,5 @@
 describe('luadbd', function()
+  assert:set_parameter('TableFormatLevel', -1)
   local parse = require('luadbd').parse
   it('fails on empty string', function()
     assert.Nil(parse(''))
@@ -6,6 +7,7 @@ describe('luadbd', function()
   it('succeeds with no columns', function()
     local expected = {
       columns = {},
+      versions = {},
     }
     assert.same(expected, parse('COLUMNS\n'))
   end)
@@ -14,6 +16,7 @@ describe('luadbd', function()
       columns = {
         { type = 'int', name = 'moocow' },
       },
+      versions = {},
     }
     assert.same(expected, parse('COLUMNS\nint moocow\n'))
   end)
@@ -23,11 +26,40 @@ describe('luadbd', function()
         { type = 'int', name = 'moocow' },
         { type = 'string', name = 'cowmoo' },
       },
+      versions = {},
     }
     assert.same(expected, parse([[
 COLUMNS
 int moocow // comment 1
 string cowmoo // comment 2
+]]))
+  end)
+  it('handles builds', function()
+    local expected = {
+      columns = {
+        { type = 'string', name = 'cowmoo' },
+      },
+      versions = {
+        {
+          builds = {
+            '7.2.0.23436-7.2.0.23514',
+            '0.7.0.3694, 0.7.1.3702, 0.7.6.3712',
+            '0.9.1.3810',
+          },
+          columns = {
+            { name = 'cowmoo' },
+          },
+        },
+      },
+    }
+    assert.same(expected, parse([[
+COLUMNS
+string cowmoo
+
+BUILD 7.2.0.23436-7.2.0.23514
+BUILD 0.7.0.3694, 0.7.1.3702, 0.7.6.3712
+BUILD 0.9.1.3810
+cowmoo
 ]]))
   end)
 end)
