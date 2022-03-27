65fddd47a4809796292ed7daf41b3a072fd0ac65
Ferronn <ferronn@ferronn.dev>
2022-03-27 02:53:13 +0000

    devcontainer

--- bin/upload.sh
+++ bin/upload.sh
@@ -2,7 +2,7 @@
 v=${1:1}
 key=${2}
 spec=luadbd-${v}-0.rockspec
-eval $(.lua/bin/luarocks path)
+eval "$(.lua/bin/luarocks path)"
 .lua/bin/luarocks install dkjson
-sed s/scm/${v}/g < luadbd-scm-0.rockspec > ${spec}
-.lua/bin/luarocks upload --skip-pack --force --temp-key ${key} ${spec}
+sed s/scm/"${v}"/g < luadbd-scm-0.rockspec > "${spec}"
+.lua/bin/luarocks upload --skip-pack --force --temp-key "${key}" "${spec}"
